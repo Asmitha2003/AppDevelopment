603b4476dbf794c88451a462626d01d194623c5b
Asmitha2003 <120015083+Asmitha2003@users.noreply.github.com>
2023-10-06 15:55:48 +0530

    Add files via upload

---
+++ Day14/water_consumption_tracker-1/src/main/java/com/example/demo/Service/LoginService.java
@@ -0,0 +1,24 @@
+package com.example.demo.Service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.demo.Entity.Login;
+import com.example.demo.Repository.LoginRepository;
+
+
+public class LoginService 
+{
+	@Autowired
+	private LoginRepository userrepo;
+	public List<Login> getUser() 
+	   {
+		  return userrepo.findAll();
+	   }
+	public void saveUser(Login le)
+	   {
+		  userrepo.save(le);
+	   }
+
+}
