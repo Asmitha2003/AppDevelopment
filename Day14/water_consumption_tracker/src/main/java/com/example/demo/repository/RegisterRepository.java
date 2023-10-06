8bf9244e03f33ce7d747c9200cf0606dd778e617
Asmitha2003 <120015083+Asmitha2003@users.noreply.github.com>
2023-10-06 15:34:59 +0530

    Delete Day14 directory

--- Day14/water_consumption_tracker/src/main/java/com/example/demo/repository/RegisterRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.entity.Register;
-
-public interface RegisterRepository extends JpaRepository<Register, Integer> {
-
-}
